36e3239bdfee04f21310b5da7311d3b1e4469ff3
Sam Magdaleno <90806960+samuel-magdaleno@users.noreply.github.com>
2022-02-11 12:55:13 -0800

    added net core, console project

---
+++ GHActionsTest/GHActionsTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GHActionsTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
